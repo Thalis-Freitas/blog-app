160ae85ee0b1ca5e24ae8dd8c70543cacfa872bb
Thalis Freitas <ofthalis@gmail.com>
2023-03-26 05:49:02 -0300

    show posts

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="post-card"
+       v-for="(post, i) in postsBlog" :key="i">
+    <h2 class="post-title">
+      {{ post.title }}
+    </h2>
+    <p class="post-body">
+      {{ post.body }}
+    </p>
+  </div>
+</template>
+
+<script>
+import blogApi from '@/services/blogApi';
+
+export default {
+  name: 'BlogPost',
+  beforeCreate(){
+    blogApi.get('/posts').then( response => {
+      this.postsBlog = response.data
+    })
+  },
+  data(){
+    return {
+      postsBlog: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-card{
+  margin: 20px auto;
+  width: 90vw;
+  padding: 40px 30px;
+  color: var(--font-color); 
+  border-radius: 10px;
+  background: rgba( 255, 255, 255, 0 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 2.5px );
+  -webkit-backdrop-filter: blur( 2.5px );
+  border: 1px solid rgba( 255, 255, 255, 0.18 );
+}
+
+.post-title{
+  position: relative;
+  bottom: 20px;
+}
+
+.post-body, .post-title{
+  text-align: left;
+}
+
+@media screen and (min-width: 750px){
+  .post-card{
+    width: 70%;
+  }
+}
+
+@media screen and (min-width: 1000px){
+  .post-card{
+    width: 55%;
+  }
+}
+</style>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,39 @@
+<template>
+  <main class="main">
+    <BlogPost/>
+  </main>
+</template>
+
+<script>
+import BlogPost from '@/components/BlogPost.vue'
+
+export default {
+    name: "MainContainer",
+    components: { BlogPost }
+}
+</script>
+
+<style scoped>
+.main{
+  position: fixed;
+  top: 5%;
+  bottom: 5%;
+  overflow-y: scroll;
+  padding: 20px;
+}
+
+.main::-webkit-scrollbar-track, .main::-webkit-scrollbar-thumb {
+  background: var(--elements-color);
+}
+
+.main::-webkit-scrollbar {
+  width: 6px;
+  background: var(--light-color);
+}
+
+.main::-webkit-scrollbar-thumb {
+  border-radius: 5px;
+  outline: 1px solid var(--light-color);
+}
+
+</style>
